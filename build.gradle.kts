a3a5d16add18007bdd2c4b7dd4f24757b76cc3bd
Eva Tatarka <eva@tatarka.me>
2024-04-15 13:44:29 -0700

    prepare release 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 }
 
 group = "me.tatarka.webpush"
-version = "0.2-SNAPSHOT"
+version = "0.1.1"
 
 val sourcesJar by tasks.registering(Jar::class) {
     archiveClassifier.set("sources")
